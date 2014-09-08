29de8dfce0dcb5d9e69755feaa9c39898cff0486
Ashwin Vasani <akvasani@asu.edu>
2014-09-08 16:32:30 -0700

    Test File
    Signed-off-by: Ashwin Vasani <akvasani@asu.edu>

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-var express = require('express')
-var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var express = require('express');
+var app = express(), bodyParser = require('body-parser');
+app.use(bodyParser());
+var http = require('http');
+var httpServer = http.Server(app);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+console.log("dir name: ",__dirname);
+app.use(express.static(__dirname+'/www'));
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/www/index.html');
+});
+
+app.get('/hi', function(req, res) {
+	console.log("hi");
+});
+
+var x, y, tx, ty;
+app.post('/data', function(req, resp){
+
+	console.log(JSON.stringify(req.body));
+	x = req.body.x;
+	y = req.body.y;
+	tx = req.body.tx;
+	ty = req.body.ty;
+	resp.writeHead(200);
+	resp.write('{"success":0}');
+	resp.end();
+
+});
+
+app.get('/data', function(req, resq) {
+	console.log("Sending x and y");
+	resq.writeHead(200);
+
+	var data1= new Object();
+	data1.x = x;
+	data1.y = y;
+	data1.tx = tx;
+	data1.ty = ty;
+	console.log("sending : " + JSON.stringify(data1));
+	resq.write(JSON.stringify(data1));
+	resq.end();
+});
+
+
+
+app.listen(3000);
