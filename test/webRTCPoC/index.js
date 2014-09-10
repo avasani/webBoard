873f10fb13e8c09923df35948aadb04c0752935d
Ashwin Vasani <akvasani@asu.edu>
2014-09-10 01:47:49 -0700

    Remove defuct code from index.js server code
    Signed-off-by: Ashwin Vasani <akvasani@asu.edu>

--- test/webRTCPoC/index.js
+++ test/webRTCPoC/index.js
@@ -10,18 +10,10 @@ app.use("/images", express.static(__dirname + '/www/images'));
 console.log("dir name: ",__dirname);
 app.use(express.static(__dirname+'/www'));
 
-
-
+/* Send the index file for "/" */ 
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/www/index.html');
 });
-
-app.get('/hi', function(req, res) {
-	console.log("hi");
-});
-
-var x = [], y = [], tx = [], ty = [];
-var idx;
 
 
 var pointX1, pointY1;
@@ -77,39 +69,5 @@ app.get('/serverData', function(request, response) {
     pointY2.clear();
 });
 
-app.post('/data', function(req, resp){
-
-	console.log(JSON.stringify(req.body));
-	if (x[idx] != req.body.x && y[idx] != req.body.y && tx[idx] != req.body.tx && ty[idx] != req.body.ty) {
-		x[idx] = req.body.x;
-		y[idx] = req.body.y;
-		tx[idx] = req.body.tx;
-		ty[idx] = req.body.ty;
-		idx++;
-	}
-	resp.writeHead(200);
-	resp.write('{"success":0}');
-	resp.end();
-
-});
-
-app.get('/data', function(req, resq) {
-	console.log("Sending x and y");
-	resq.writeHead(200);
-
-	if (idx != 0 ) {
-		var data1= new Object();
-		data1.x = x[idx - 1];
-		data1.y = y[idx - 1];
-		data1.tx = tx[idx - 1];
-		data1.ty = ty[idx - 1];
-		idx--;
-		console.log("sending : " + JSON.stringify(data1));
-		resq.write(JSON.stringify(data1));
-	} else {
-		resq.write("{'data':'no data'");
-	}
-	resq.end();
-});
-
 app.listen(((process.env.PORT || 5000)));
+//app.listen(3000);
